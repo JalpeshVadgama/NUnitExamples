d54d55e71c64f7443018b7685e8f1c41ad872f7a
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-07-08 00:58:29 +0530

    Added calculator classes.

--- NunitSampleLibrary/NunitSampleLibrary/Class1.cs
+++ NunitSampleLibrary/NunitSampleLibrary/Class1.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace NunitSampleLibrary
 {
-    public class Class1
+    public class Calculator
     {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
