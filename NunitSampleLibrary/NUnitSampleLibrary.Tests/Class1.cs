d0bf97ec5228d1557b26ebdc488afe9459c8139b
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-07-08 01:05:42 +0530

    Added nunit Test project

---
+++ NunitSampleLibrary/NUnitSampleLibrary.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitSampleLibrary.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void ShouldAddNumber()
+        {
+            
+        }
+    }
+}
