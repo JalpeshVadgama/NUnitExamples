1b392571c1fac8555735c0809604c72d60986a00
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-07-09 00:49:13 +0530

    Added test methods for subtraction, multiplication and division

--- NunitSampleLibrary/NUnitSampleLibrary.Tests/CalculatorTests.cs
+++ NunitSampleLibrary/NUnitSampleLibrary.Tests/CalculatorTests.cs
@@ -14,5 +14,29 @@ namespace NUnitSampleLibrary.Tests
             //Assert.AreEqual(60, result);
             Assert.That(result, Is.EqualTo(60));
         }
+
+        [Test]
+        public void AddSubstractTest()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Substract(50, 10);
+            Assert.That(result, Is.EqualTo(40));
+        }
+
+        [Test]
+        public void AddMultiplyTest()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Multiply(10, 50);
+            Assert.That(result, Is.EqualTo(500));
+        }
+
+        [Test]
+        public void AddDevideTest()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Device(50, 10);
+            Assert.That(result, Is.EqualTo(5));
+        }
     }
 }
